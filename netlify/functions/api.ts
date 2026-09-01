7bc15d9d90711f9754a9525bc733a9ab7f1c6d97
devalamalsafi-netizen <devalamalsafi@gmail.com>
2026-09-01 22:09:49 +0100

    Update api.ts

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -2,4 +2,4 @@ import serverless from "serverless-http";
 
 import { createServer } from "../../server";
 
-export const handler = serverless(createServer());
+export default async (req: Request): Promise<Response> => {
