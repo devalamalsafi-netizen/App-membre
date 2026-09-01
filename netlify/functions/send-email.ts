f224a45540edea5793658222d5e1ad06aad3a875
devalamalsafi-netizen <devalamalsafi@gmail.com>
2026-09-01 22:07:43 +0100

    Update send-email.ts

--- netlify/functions/send-email.ts
+++ netlify/functions/send-email.ts
@@ -1,6 +1,6 @@
 import { handleSendEmail } from "../../server/routes/email";
 import { runExpressHandler } from "./_shared/express-shim";
 
-export const handler = async (req: Request): Promise<Response> => {
+export default async (req: Request): Promise<Response> => {
   return runExpressHandler(handleSendEmail, req);
 };
