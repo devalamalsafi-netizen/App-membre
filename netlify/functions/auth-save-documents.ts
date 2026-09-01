fd30da9bac1ab8eef08bd89694a825966d6989b4
devalamalsafi-netizen <devalamalsafi@gmail.com>
2026-09-01 22:08:32 +0100

    Update auth-save-documents.ts

--- netlify/functions/auth-save-documents.ts
+++ netlify/functions/auth-save-documents.ts
@@ -1,6 +1,6 @@
 import { handleSavePdfQrCode } from "../../server/routes/auth";
 import { runExpressHandler } from "./_shared/express-shim";
 
-export const handler = async (req: Request): Promise<Response> => {
+export default async (req: Request): Promise<Response> => {
   return runExpressHandler(handleSavePdfQrCode, req);
-};
+}
